90c7463b883f260b922c948ac127bb83b6890557
Celeste Fournier <celeste.fournier@fanatee.com>
2023-08-07 02:41:11 -0300

    Add trail when touch the screen

---
+++ Assets/Scripts/Gameplay/TouchTrailController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class TouchTrailController : MonoBehaviour
+    {
+        [SerializeField] List<TrailRenderer> Trails;
+        [SerializeField] Camera Camera;
+
+        const int MaxTrails = 5;
+
+        void Update()
+        {
+            if (Input.touchCount <= 0)
+                return;
+
+            for (var i = 0; i < Input.touches.Length; i++)
+            {
+                if (i >= MaxTrails)
+                    return;
+
+                var touch = Input.GetTouch(i);
+                var touchPosition = Camera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y,
+                    Camera.nearClipPlane));
+
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        Trails[i].transform.position = touchPosition;
+                        Trails[i].Clear();
+                        break;
+                    case TouchPhase.Moved:
+                        Trails[i].transform.position = touchPosition;
+                        break;
+                }
+            }
+        }
+    }
+}
